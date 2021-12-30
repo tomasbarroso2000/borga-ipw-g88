5b5ecfab99a92b63870e8a2d5698ea0b65dd19d0
tomasbarroso2000 <tomasbarroso2000@gmail.com>
2021-12-30 20:00:24 +0000

    Get groups style refactor

--- board-games-data.js
+++ board-games-data.js
@@ -12,7 +12,6 @@ async function getMechanics(mechanics) {
 	const ids = mechanics.map(data => data.id);
 	const search_mechanics_uri = BOARD_GAME_ATLAS_BASE_URI + 'game/mechanics?pretty=true&client_id=' + ATLAS_CLIENT_ID;
 	const response = await do_fetch(search_mechanics_uri);
-	console.log(response);
 	const filtered = response.mechanics.filter(m => ids.includes(m.id));
 	const names = filtered.map(data => data.name);
 	return names;
@@ -22,7 +21,6 @@ async function getCategories(categories) {
 	const ids = categories.map(data => data.id);
 	const search_categories_uri = BOARD_GAME_ATLAS_BASE_URI + 'game/categories?pretty=true&client_id=' + ATLAS_CLIENT_ID;
 	const response = await do_fetch(search_categories_uri);
-	console.log(response);
 	const filtered = response.categories.filter(c => ids.includes(c.id));
 	const names = filtered.map(data => data.name);
 	return names;
